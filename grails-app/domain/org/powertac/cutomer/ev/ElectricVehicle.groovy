080105fba89b31299f8628409f0b1101855d65ad
ddauer <ddauer@gmail.com>
2011-09-04 21:58:00 +0200

    Data collection

--- grails-app/domain/org/powertac/cutomer/ev/ElectricVehicle.groovy
+++ grails-app/domain/org/powertac/cutomer/ev/ElectricVehicle.groovy
@@ -10,8 +10,12 @@ import org.powertac.common.TariffSubscription
 import org.powertac.common.enumerations.PowerType
 import org.powertac.common.enumerations.CustomerType
 import org.powertac.common.CustomerInfo
+import org.joda.time.Instant
+import org.powertac.common.Tariff
 
 class ElectricVehicle extends AbstractCustomer {
+
+  // NOTE: Competition start time should be set to a Monday, 0:00 in Competition.groovy or Web UI!
 
   String name
   BigDecimal capacity_kwh = 35.0 // default value if not found in config
@@ -30,90 +34,6 @@ class ElectricVehicle extends AbstractCustomer {
     config(nullable: false)
   }
 
-  // Load the driving profile
-//  def loadData() {
-//    DateTime competitionBaseTime
-//    competitionBaseTime = Competition.currentCompetition().simulationBaseTime.toDateTime()
-//    // Assume Mon, 2011/07/04 for evaluation purposes
-//    //competitionBaseTime = new DateTime(2011, 7, 4, 0, 0, 0, 0, DateTimeZone.UTC)
-//
-//    // Load generated driving profile
-//    CSVReader reader = new CSVReader(new FileReader(config?.configuration?.drivingProfile))
-//    String[] nextLine
-//    String[] previousLine
-//
-//    // Count lines since we need to group 4 lines each
-//    def count = 0
-//
-//    // Keep track of timeslots
-//    ElectricVehicleTimeslot previousTimeslot
-//    ElectricVehicleTimeslot currentTimeslot
-//
-//    while ((nextLine = reader.readNext()) != null) {
-//      // Skip first line (header)
-//      if (nextLine[0] == "ID") { continue }
-//
-//      // Extract values from csv
-//      //def id = nextLine[0].stripIndent() // not needed
-//      //def jobType = nextLine[1].stripIndent()
-//      //def weekday = nextLine[2].stripIndent()
-//      //def date = nextLine[3].stripIndent()
-//      def km = nextLine[4].stripIndent() as BigDecimal
-//      def tripType = nextLine[5].stripIndent() as String
-//
-//      // Create timeslot if necessary
-//      if (count == 0) { // :00
-//        if (previousTimeslot) {
-//          currentTimeslot = new ElectricVehicleTimeslot(dateTime: previousTimeslot.dateTime.plusHours(1))
-//          currentTimeslot.trip = tripType
-//          currentTimeslot.atHome = previousTimeslot.atHome
-//          currentTimeslot.driving = (tripType != "NoTrip")
-//          currentTimeslot.charging = false
-//          currentTimeslot.km = km
-//
-//          if (previousTimeslot.trip == "ToHome" && tripType == "NoTrip") { // check if we are at home
-//            currentTimeslot.atHome = true
-//          }
-//
-//          currentTimeslot.save()
-//          this.addToTimeslots(currentTimeslot)
-//        } else {
-//          // Timeslot 0, time based on comeptitionBaseTime
-//          currentTimeslot = new ElectricVehicleTimeslot(dateTime: competitionBaseTime.plusHours(1))
-//          // Since this is the very first timeslot, we can reason the following values
-//          currentTimeslot.trip = tripType
-//          currentTimeslot.atHome = (tripType == "NoTrip")
-//          currentTimeslot.driving = (tripType != "NoTrip")
-//          currentTimeslot.charging = false
-//          currentTimeslot.stateOfCharge = config?.configuration?.capacity_kwh as BigDecimal
-//          currentTimeslot.save()
-//          this.addToTimeslots(currentTimeslot)
-//        }
-//
-//
-//      } else { // :15, :30, :45 - update within timeslot
-//        currentTimeslot.km += km // Sum km up
-//
-//        if (tripType != "NoTrip") { // Leave timeslot state untouched unless we are not at home (!= "NoTrip").
-//          currentTimeslot.atHome = false
-//          currentTimeslot.driving = true
-//          currentTimeslot.trip = tripType
-//        }
-//
-//        currentTimeslot.save()
-//      }
-//
-//
-//      count++
-//      if (count == 4) {
-//        count = 0
-//      }
-//
-//      previousLine = nextLine
-//      previousTimeslot = currentTimeslot
-//    }
-//  }
-
   void configure(PluginConfig config) {
     this.config = config
     name = config.name
@@ -129,11 +49,16 @@ class ElectricVehicle extends AbstractCustomer {
     // Every customer needs to have a customer info
     CustomerInfo info = new CustomerInfo(name: name,
         customerType: CustomerType.CustomerElectricVehicle,
-        powerTypes: [PowerType.CONSUMPTION]).save()
+        powerTypes: [PowerType.CONSUMPTION]) // PowerTypes never saved correctly! Workaround in CustomerInfo.
+
+     if (!info.validate()) {
+      info.errors.allErrors.each {log.error(it.toString())}
+    }
+    info.save()
 
     this.customerInfo = info
     this.custId = customerInfo.getId()
-
+    this.save()
   }
 
   String getValidConfig(PluginConfig config, String name, String defaultValue) {
@@ -148,8 +73,8 @@ class ElectricVehicle extends AbstractCustomer {
   }
 
   /*
-  Overriding this is required so we can report our own power consumption
-   */
+ Overriding this is required so we can report our own power consumption
+  */
 
   @Override
   void step() {
@@ -160,42 +85,34 @@ class ElectricVehicle extends AbstractCustomer {
 
   @Override
   void consumePower() {
+    // Collect decision data in addition to existing instance variables (soc, thresholds)
 
-    // HEURISTIC HERE
+    // Get current time
+    Instant now = timeService.currentTime
+
+    // Get current price
+    // Assumption: There is only one (default) tariff available
+    List <TariffSubscription> subscriptionList = subscriptions as List
+    if (subscriptionList.size() > 1) {
+      log.error "there should be only one subscription: ${subscriptionList}"
+    }
+    TariffSubscription subscription = subscriptionList.get(0)
+    Tariff tariff = subscription.tariff
+    double price = tariff.getUsageCharge(now)
+
+    log.error "price is ${price}"
+
+    // Determine columns
+    int distanceColumn = profileId*2
+    int typeColumn = distanceColumn + 1
+
+    log.error "check columns ${distanceColumn} and ${typeColumn}"
+
+    // Report power usage
+    // use bigDecimalInstance.toDouble()
+    subscription.usePower(123.123)
 
 
-    DateTime currentDateTime = timeService.getCurrentDateTime()
-    int day = currentDateTime.getDayOfWeek()
-    int hour = currentDateTime.getHourOfDay()
-
-    // Find corresponding EVTimeslot
-//    ElectricVehicleTimeslot evts
-//    Boolean found = false
-//    ElectricVehicleTimeslot.getAll().each { ts ->
-//      if (!found) {
-//        DateTime dt = ts.dateTime
-//        if (dt.getDayOfWeek() == day && dt.getHourOfDay() == hour) {
-//          evts = ts
-//          found = true
-//        }
-//      }
-//    }
-//
-//    if (!found) {
-//      log.error "No timeslot found"
-//    }
-//
-//    // There should be only 1 subscription
-//    def subscriptionList = subscriptions as List
-//
-//    if (subscriptionList.size() == 1) {
-//      TariffSubscription sub = subscriptionList.get(0)
-//      double power = evts?.energyDemand?.toDouble()
-//      sub.usePower(power)
-//      log.info "EV using $power"
-//    } else {
-//      log.error "More than 1 subscription"
-//    }
   }
 
   @Override
